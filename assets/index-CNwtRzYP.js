41432a89d1b8fdd79b42bf2f6d281cd09a01338c
Julian Schievelkamp <julian.schievelkamp@gmail.com>
2025-04-06 11:07:10 +0200

    Updates

--- assets/index-CNwtRzYP.js
+++ assets/index-CNwtRzYP.js
@@ -330,7 +330,7 @@ Error generating stack: `+e.message+`
 
     img,
     video {
-        max-height: calc(100vh - 14.5rem);
+        max-height: calc(var(--100vh) - 14.5rem);
         border: 8px solid black;
     }
 
