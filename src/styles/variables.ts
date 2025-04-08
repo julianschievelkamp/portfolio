80f7e5d27f418831eeeb1ef2e9834c42cd517c13
Julian Schievelkamp <julian.schievelkamp@gmail.com>
2025-04-08 16:39:41 +0200

    remove unused code

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -28,29 +28,16 @@ export const darkTheme: Theme = {
 };
 
 export const queries = {
-    xs: "screen and (min-width: 350px)",
     sm: "screen and (min-width: 576px)",
     md: "screen and (min-width: 768px)",
     lg: "screen and (min-width: 992px)",
     xl: "screen and (min-width: 1200px)",
-    xxl: "screen and (min-width: 1600px)",
-    xxxl: "screen and (min-width: 2000px)",
     hover: "(hover: hover)",
-};
-
-export const borderRadius = {
-    round: "100px",
-    edged: "10px",
 };
 
 export const transition = {
     fastest: "100ms ease-out",
     fast: "300ms ease-out",
-};
-
-export const shadow = {
-    soft: "0 0 8px 0 rgba(0, 0, 0, 0.1)",
-    hard: "2px 2px 3px 0 rgba(0, 0, 0, 0.2)",
 };
 
 export const scrollStyles = css`
