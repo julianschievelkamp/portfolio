34b1780c0c9c15d5e6a5f6151b3bb39ce234935a
Julian Schievelkamp <julian.schievelkamp@gmail.com>
2025-04-08 14:19:41 +0200

    try fix zoom bug

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -28,7 +28,7 @@ export default createGlobalStyle<{ theme?: Theme }>`
 export const set100vh = () => {
     let value = "100vh";
 
-    if (window.innerHeight) {
+    if (window.innerWidth && window.innerWidth === screen.width) {
         value = `${window.innerHeight}px`;
     }
 
