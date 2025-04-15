3857b9466ab20f12170b3154a17e68e4efeda39d
Julian Schievelkamp <julian.schievelkamp@gmail.com>
2025-04-15 16:50:58 +0200

    change meta title

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -4,7 +4,7 @@ import vikeReactStyledComponents from "vike-react-styled-components/config";
 export default {
     prerender: true,
     extends: [vikeReact, vikeReactStyledComponents],
-    title: "Portfolio - Julian Schievelkamp",
+    title: "Julian Schievelkamp: Portfolio",
     description:
         "JULIAN SCHIEVELKAMP CGN #1994. Visual artist and fine art photographer highlighting the synergies of classic photography and CGI.",
     image: "https://www.julianschievelkamp.com/og-image.jpg",
