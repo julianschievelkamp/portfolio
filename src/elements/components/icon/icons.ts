dfb7d481a1b7f2a718a5921a07cf677aa11df3dc
Julian Schievelkamp <julian.schievelkamp@gmail.com>
2026-09-15 14:32:17 +0200

    delete unused icons

--- src/elements/components/icon/icons.ts
+++ src/elements/components/icon/icons.ts
@@ -1,18 +1,12 @@
 /// <reference types="vite-plugin-svgr/client" />
-import Dark from "assets/icons/darkmode.svg?react";
-import Light from "assets/icons/lightmode.svg?react";
 import Close from "assets/icons/close.svg?react";
 import Menu from "assets/icons/menu.svg?react";
-import Instagram from "assets/icons/instagram.svg?react";
 import ChevronLeft from "assets/icons/chevron-left.svg?react";
 import ChevronRight from "assets/icons/chevron-right.svg?react";
 
 export const icons = {
-    dark: Dark,
-    light: Light,
     close: Close,
     menu: Menu,
-    instagram: Instagram,
     chevronLeft: ChevronLeft,
     chevronRight: ChevronRight,
 };
