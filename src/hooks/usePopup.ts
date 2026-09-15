df0730fa1e3de91b6794fadb032714c871be8221
Julian Schievelkamp <julian.schievelkamp@gmail.com>
2026-09-15 13:15:58 +0200

    prevent swipe if zoomed in

--- src/hooks/usePopup.ts
+++ src/hooks/usePopup.ts
@@ -37,6 +37,12 @@ export const usePopup = (items: PortfolioItem[]) => {
 
         if (!touchStart || !touchEnd) return;
 
+        if (typeof window !== "undefined" && window.visualViewport) {
+            if (Math.abs(window.visualViewport.scale - 1) > 0.05) {
+                return;
+            }
+        }
+
         const minSwipeDistance = 50;
         const distance = touchStart - touchEnd;
         const isLeftSwipe = distance > minSwipeDistance;
