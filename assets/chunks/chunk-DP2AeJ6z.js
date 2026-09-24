bd0eabea5fc330230f6cc23edad870f2a4d767dc
Julian Schievelkamp <julian.schievelkamp@gmail.com>
2026-09-24 16:04:50 +0200

    Updates

--- assets/chunks/chunk-DP2AeJ6z.js
+++ assets/chunks/chunk-DP2AeJ6z.js
@@ -389,7 +389,7 @@ Error generating stack: `+e.message+`
     //visibility: ${({$isOpen:e})=>e?`visible`:`hidden`};
     opacity: ${({$isOpen:e})=>+!!e};
     pointer-events: ${({$isOpen:e})=>e?`all`:`none`};
-    transition: ${cr.fast} allow-discrete;
+    transition: ${cr.fast};
     width: 100%;
     height: 100%;
     padding: 1rem;
